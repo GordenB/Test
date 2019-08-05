9bf22d4abe5d2817728872a7d1199d953a343596
Gorden Biemüller <Gorden.biem@Googlemail.com>
2019-08-05 10:42:04 +0200

    add CSS

--- src/main/java/de/Gorden/Application.java
+++ src/main/java/de/Gorden/Application.java
@@ -3,7 +3,6 @@ package de.Gorden;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import ch.qos.logback.core.db.dialect.MySQLDialect;
 
 @SpringBootApplication
 public class Application {
